e4d5c044c6f4319b6413d11857daf741287150bc
Todd Hainsworth <todd@staff.mindvision.com.au>
2014-03-19 15:25:59 +1030

    hook up devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!
+
   protected
 
   def configure_permitted_parameters
